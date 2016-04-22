916e5768fd6ec14f7da348a65608dec3b9978ece
Michał Stempkowski <michulix@gmail.com>
2016-04-22 07:26:17 +0200

    test

--- unit/menu/engine/MainMenuTest.java
+++ unit/menu/engine/MainMenuTest.java
@@ -5,14 +5,11 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-/**
- * Created by Dell on 22.04.2016.
- */
 public class MainMenuTest {
     @Test
     public final void shouldHaveProperNaming() throws Exception {
         Menu m = new MainMenu();
-        assertThat(m.toString(), is(equalTo("MainMenu instance")));
+        assertThat(m.toString(), is(not(equalTo("MainMenu instance"))));
     }
 
 }

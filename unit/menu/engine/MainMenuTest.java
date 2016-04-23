940fae56ad7e3cb86c7c58f2edbb6243864ec305
Michał Stempkowski <michulix@gmail.com>
2016-04-23 13:45:52 +0200

    travis final test

--- unit/menu/engine/MainMenuTest.java
+++ unit/menu/engine/MainMenuTest.java
@@ -9,7 +9,7 @@ public class MainMenuTest {
     @Test
     public final void shouldHaveProperNaming() throws Exception {
         Menu m = new MainMenu();
-        assertThat(m.toString(), is(equalTo("MainMenu instance")));
+        assertThat(m.toString(), is(equalTo("MainMenuy instance")));
     }
 
 }
